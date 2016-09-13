0638b5a5e1db4bf5fda293bc4ca9193c9b81f270
Susan Graves <susan@susangravescpa.com>
2016-09-13 16:03:08 -0400

    1st test written

--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -5,3 +5,15 @@
 #eg - if you have 11 cents you should return {:dime => 1, :penny = 1}; if you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny =>1}.
 
 #Build up your coin_changer using tests.  Commit to git after every test.
+
+
+require "minitest/autorun"
+require_relative "coin_changer.rb"
+
+class TestCoinChanger < Minitest::Test
+
+	def test_0_cents_returns_empty_hash
+		cents_received = 0
+		assert_equal({},coin_changer(cents_received))
+	end
+end
