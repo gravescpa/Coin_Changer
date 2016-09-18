b6819871f032786d1f7f46588e595c345f13bb49
Susan Graves <susan@susangravescpa.com>
2016-09-18 13:00:53 -0400

    Tried to add a user input for cents but it doesn't work

--- coin_changer.rb
+++ coin_changer.rb
@@ -11,10 +11,18 @@
 # 2) make the penny if statement exactly as the others
 # 3) see if you can find a way to only have a single if statement (think iteration)
 
+# Tried to add a user input but it doesn't work
+# puts "I can tell you the combination of the least amount of coins for the number of cents you have"
+# print "How many cents do you have?"
+# cents_received = gets.chomp
+
+
+
 def coin_changer(cents_received)
+
 	coins = {}
 
-	coins_value = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
+	coins_value = {:sacagawea => 100, :half_dollar => 50, :quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
 	
 	coins_value.each do |coin, value|
 
@@ -42,4 +50,6 @@ end
 
 	# end
 
+coins = coin_changer(352)
+puts coins
 
