e158e7902543fbde318b22d39d20975724498ba1
Susan Graves <susan@susangravescpa.com>
2016-09-27 14:20:53 -0400

    pulls values out of hash

--- app.rb
+++ app.rb
@@ -19,24 +19,13 @@ post '/cents' do
 	name = params[:user_name].capitalize
 	cents = params[:cents].to_i
 	coins = coin_changer(cents)
-	"The least amount of change I can give you for #{cents} cents is #{coins}"
-	# redirect '/3_fav_num?name=' + name + '&age=' + age
+# "The least amount of change I can give you for #{cents} cents is #{coins}"
+
+
+"The least amount of change I can give you for #{cents} cents is: #{coins[:quarter]} quarter(s), #{coins[:nickel]} nickel(s), #{coins[:dime]} dime(s), and #{coins[:penny]} penny(s)."
+	
 end
 
-# get '/3_fav_num' do
-# 	name = params[:name].capitalize
-# 	age = params[:age]
-# 	erb :get_fav_num, :locals => {:name => name, :age => age}
-# end
 
-# post '/3_fav_num' do
-# 	age = params[:age]
-# 	name = params[:user_name].capitalize
-# 	number1 = params[:number1].to_i
-# 	number2 = params[:number2].to_i
-# 	number3 = params[:number3].to_i
-# 	sum = add(number1, number2, number3)
-# 	"Well, #{name}, at age #{age}, your three favorite numbers are: #{number1}, #{number2}, and #{number3}.  Their total is #{sum}."
-# end
 
 
--- coin_changer.rb
+++ coin_changer.rb
@@ -11,7 +11,7 @@
 # 2) make the penny if statement exactly as the others
 # 3) see if you can find a way to only have a single if statement (think iteration)
 
-# Tried to add a user input but it doesn't work
+#Tried to add a user input but it doesn't work
 # puts "I can tell you the combination of the least amount of coins for the number of cents you have"
 # 	print "How many cents do you have? "
 # 	cents_received(gets.chomp)
@@ -20,9 +20,11 @@
 
 def coin_changer(cents_received)
 
-	puts "I can tell you the combination of the least amount of coins for the number of cents you have"
-	print "How many cents do you have? "
-	cents_received(gets.chomp)
+	# puts "I can tell you the combination of the least amount of coins for the number of cents you have"
+	
+	# print "How many cents do you have? "
+	
+	# cents_received(gets.chomp)
 
 	coins = {}
 
@@ -36,11 +38,26 @@ def coin_changer(cents_received)
 
 		cents_received = cents_received % value
 
+
+
 	end
 
-	coins
+	#coins
+	# coins.each do |key, value|
+	# 		if value == nil
+	# 			value = 0
+	# 		end
+	# 	end
+	coins	
+	# coins.each do |name, value|
+	# 	if value >= 1
+
 	
 end
+
+
+# coins = coin_changer()
+# puts coins
 
 	#This is the first round of attempts
 	# if cents_received.between?(6,9)
@@ -54,8 +71,4 @@ end
 	# 	coins[:penny] = cents_received
 
 	# end
-
-
-coins = coin_changer(cents_received)
-puts coins
 
